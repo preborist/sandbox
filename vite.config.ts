ef6f643a7f6eed0403e19e9d98a52a96ae8dc4ca
serhii-koziuba <preborist@gmail.com>
2025-11-30 13:48:42 +0200

    Revert "Update build script and set Vite base path for production"
    This reverts commit 291675d98b176c61bd7e798c109843260d701470.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const repoName = 'sandbox'; 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
@@ -13,5 +10,4 @@ export default defineConfig({
     port: 5173,
     strictPort: true,
   },
-  base: isProd ? `/${repoName}/` : '/',
 });
